90b8746eb5b742c49b3788f83f0074ce6fe57f42
Denys Lemeshko <lemden@gmail.com>
2018-04-22 14:01:11 -0700

    qsort

--- datastructures/arrays/algorithms.ts
+++ datastructures/arrays/algorithms.ts
@@ -53,3 +53,46 @@ export class Number implements ComparableNode<number,number> {
         return arr.map(n => new Number(n));
     }
 }
+
+const swap = <K,V>(arr: Array<ComparableNode<K,V>>, i: number, j: number) => {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    return arr;
+}
+
+const partition = <K,V>(arr: Array<ComparableNode<K,V>>, left: number, right: number) => {
+    const middleIndex = Math.floor( (left + right) / 2 );
+    const middle: ComparableNode<K,V> = arr[middleIndex];
+    let leftSide = left;
+    let rightSide = right;
+    while (leftSide <= rightSide) {
+        while (arr[leftSide].getKey().less(middle.getKey().getValue())) {
+            leftSide ++;
+        }
+        while (arr[rightSide].getKey().more(middle.getKey().getValue())) {
+            rightSide --;
+        }
+        if (leftSide >= rightSide) {
+            break;
+        }
+        
+        swap(arr, leftSide, rightSide);
+        leftSide ++;
+        rightSide --;
+    }
+
+    return rightSide;
+}
+
+const QSortWithLeftRight = <K,V>(arr: Array<ComparableNode<K,V>>, left: number, right: number) => {
+    if (left < right) {
+        const q = partition(arr, left, right);
+        QSortWithLeftRight(arr, left, q);
+        QSortWithLeftRight(arr, q + 1, right);
+    }
+};
+
+export const QSort = <K,V>(arr: Array<ComparableNode<K,V>>) => {
+    QSortWithLeftRight(arr, 0, arr.length - 1);
+}
--- datastructures/arrays/index.ts
+++ datastructures/arrays/index.ts
@@ -1,7 +1,13 @@
 import * as Alg from "./algorithms";
-const arr = Alg.Number.fromArray([1,2,3,4,5,6,7,8,9,10,11]);
-console.log(Alg.BinarySearch(arr, -1));
-console.log(Alg.BinarySearch(arr, 10));
-console.log(Alg.BinarySearch(arr, 5));
-console.log(Alg.BinarySearch(arr, 11));
-console.log(Alg.BinarySearch(arr, 2));
+
+// const arr = Alg.Number.fromArray([1,2,3,4,5,6,7,8,9,10,11]);
+// console.log(Alg.BinarySearch(arr, -1));
+// console.log(Alg.BinarySearch(arr, 10));
+// console.log(Alg.BinarySearch(arr, 5));
+// console.log(Alg.BinarySearch(arr, 11));
+// console.log(Alg.BinarySearch(arr, 2));
+
+let a = 1;
+const arr2 = Alg.Number.fromArray([1000,127,3,12,4,5,6,54,100]);
+Alg.QSort(arr2);
+console.log(arr2);
