98486f2c14d77d73b24c9ff324ff11cab6675f8b
Denys Lemeshko <lemden@gmail.com>
2018-04-22 16:21:22 -0700

    mege sort

--- datastructures/arrays/algorithms.ts
+++ datastructures/arrays/algorithms.ts
@@ -96,3 +96,33 @@ const QSortWithLeftRight = <K,V>(arr: Array<ComparableNode<K,V>>, left: number, 
 export const QSort = <K,V>(arr: Array<ComparableNode<K,V>>) => {
     QSortWithLeftRight(arr, 0, arr.length - 1);
 }
+
+export const MergeSorted = <K,V>(arr1: Array<ComparableNode<K,V>>, arr2: Array<ComparableNode<K,V>>) => {
+    const result: Array<ComparableNode<K,V>> = [];
+    let i = 0, j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i].getKey().less(arr2[j].getKey().getValue())){
+            result.push(arr1[i++]);
+        } else {
+            result.push(arr2[j++]);
+        }
+    }
+    while (i<arr1.length) {
+        result.push(arr1[i++]);
+    }
+    while (j<arr2.length) {
+        result.push(arr2[j++]);
+    }
+    return result;
+}
+
+export const MSort = <K,V>(arr: Array<ComparableNode<K,V>>) => {
+    if (arr.length === 1) {
+        return arr;
+    }
+    const middle = Math.floor(arr.length / 2);
+    return MergeSorted(
+        MSort(arr.slice(0, middle)),
+        MSort(arr.slice(middle, arr.length))
+    );
+}
--- datastructures/arrays/index.ts
+++ datastructures/arrays/index.ts
@@ -7,7 +7,15 @@ import * as Alg from "./algorithms";
 // console.log(Alg.BinarySearch(arr, 11));
 // console.log(Alg.BinarySearch(arr, 2));
 
-let a = 1;
-const arr2 = Alg.Number.fromArray([1000,127,3,12,4,5,6,54,100]);
-Alg.QSort(arr2);
-console.log(arr2);
+// let a = 1;
+// const arr2 = Alg.Number.fromArray([1000,127,3,12,4,5,6,54,100]);
+// Alg.QSort(arr2);
+// console.log(arr2);
+
+// const arr1 = Alg.Number.fromArray([1,3,5,7,9]); 
+// const arr2 = Alg.Number.fromArray([0,2,4,6,8]); 
+// const merged = Alg.MergeSorted(arr1, arr2);
+// console.log(merged);
+
+const merged = Alg.MSort(Alg.Number.fromArray([6,5,2,10,1,12]));
+console.log(merged);
