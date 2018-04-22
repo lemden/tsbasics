12a06daeb5ec3a9c08020f82b3654a450eb0f54e
Denys Lemeshko <lemden@gmail.com>
2018-04-21 22:38:13 -0700

    max heap

---
+++ datastructures/graphs/trees/heaps/BinaryMaxHeap.ts
@@ -0,0 +1,92 @@
+export interface Comparable<T> {
+    more(c: Comparable<T>): boolean;
+    less(c: Comparable<T>): boolean;
+    getValue(): T;
+}
+
+export default
+class BinaryMaxHeap<V, T extends Comparable<V>> {
+    private list: T[];
+    constructor(list: T[] = null){
+        this.list = list || [];
+        this.heapify();
+    }
+
+    public append(heap: BinaryMaxHeap<V,T>) {
+        this.list = this.list.concat(heap.list);
+        this.heapify();
+    }
+
+    private printme(){
+        console.log(this.list);
+    }
+
+    private heapify(){
+        for (let i = Math.floor(this.list.length / 2); i >= 0 ; i--) {
+            this.shiftDown(i);
+        }
+    }
+
+    public add(value: T) {
+        this.list.push(value);
+        const i = this.list.length - 1;
+        this.shiftUp(i);
+    }
+
+    public next(): T {
+        if (this.list.length > 1) {
+            const root = this.list[0];
+            this.list[0] = this.list.pop();
+            this.shiftDown(0);
+            return root;
+        } else if (this.list.length === 1) {
+            const result = this.list[0];
+            this.list = [];
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    private shiftUp(i: number) {
+        let currentIndex = i;
+        while (currentIndex > 0) {
+            const parentIndex =  Math.floor( (currentIndex - 1) / 2 );
+            if (this.list[parentIndex].less(this.list[currentIndex])) {
+                this.swap(parentIndex, currentIndex);
+            }
+            currentIndex = parentIndex;
+        }
+    }
+
+    private shiftDown(i: number) {
+        let currentIndex = i;
+        while (currentIndex < this.list.length) {
+            const leftIndex = 2 * currentIndex + 1;
+            const rightIndex = 2 * currentIndex + 2;
+
+            if (leftIndex >= this.list.length) {
+                break;
+            }
+            let target = leftIndex;
+            if (rightIndex < this.list.length 
+                    && this.list[rightIndex].more(this.list[leftIndex])) {
+                target = rightIndex;
+            }
+            if (this.list[currentIndex].less(this.list[target])) {
+                this.swap(currentIndex, target);
+                currentIndex = target;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private swap(i1: number, i2: number) {
+        if (i1 < this.list.length && i2 < this.list.length) {
+            const temp: T = this.list[i1];
+            this.list[i1] = this.list[i2];
+            this.list[i2] = temp;
+        }
+    }
+}
---
+++ datastructures/graphs/trees/heaps/index.ts
@@ -0,0 +1,55 @@
+import BinaryMaxHeap, {Comparable} from "./BinaryMaxHeap";
+
+export class HeapNode implements Comparable<number> {
+    private value: number;
+    constructor(value: number) {
+        this.value = value;
+    }
+    public getValue(): number{
+        return this.value;
+    }
+    public more(node: Comparable<number>) {
+        return this.value > node.getValue();
+    }
+    public less(node: Comparable<number>) {
+        return this.value < node.getValue();
+    }
+    public static fromArray(numbers: number[]): HeapNode[]{
+        return numbers.map(n => new HeapNode(n));
+    }
+}
+
+// const heap1 = new BinaryMaxHeap<number, HeapNode>();
+// heap1.add(new HeapNode(100));
+// heap1.add(new HeapNode(200));
+// heap1.add(new HeapNode(300));
+// heap1.add(new HeapNode(50));
+// heap1.add(new HeapNode(75));
+// heap1.add(new HeapNode(95));
+// heap1.add(new HeapNode(195));
+// heap1.add(new HeapNode(1000));
+// heap1.add(new HeapNode(15));
+// heap1.add(new HeapNode(27));
+let next;
+// while ( next = heap1.next()) {
+//     console.log(next.getValue());
+// }
+// ----
+
+// const heap2 = new BinaryMaxHeap<number, HeapNode>(
+//     HeapNode.fromArray([111, 20, 32, 500, 21, 1003, 1244, 5, 99, 13, 77])
+// );
+// while ( next = heap2.next()) {
+//     console.log(next.getValue());
+// }
+
+const heap31 = new BinaryMaxHeap<number, HeapNode>(
+    HeapNode.fromArray([111, 20, 32, 500, 21])
+);
+const heap32 = new BinaryMaxHeap<number, HeapNode>(
+    HeapNode.fromArray([1003, 1244, 5, 99, 13, 77])
+);
+heap31.append(heap32);
+while ( next = heap31.next()) {
+    console.log(next.getValue());
+}
